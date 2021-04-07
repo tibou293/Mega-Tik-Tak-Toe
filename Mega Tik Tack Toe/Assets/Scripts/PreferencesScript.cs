5b1562cb57c8477b8e7725e1114e3a432d008feb
tibou293 <ti@schneider-dratz.de>
2021-04-07 22:45:53 +0200

    Different changes
    new turn icons

---
+++ Mega Tik Tack Toe/Assets/Scripts/PreferencesScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PreferencesScript : MonoBehaviour
+{
+    public GameObject PreferencesPanel;
+    public GameObject SoundeffectToggle;
+    public GameObject AudioScript;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PreferencesPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    //open and close Preferences
+    public void openPreferences()
+    {
+        if (PreferencesPanel.activeSelf == false)
+        {
+            PreferencesPanel.SetActive(true);
+        }
+        else
+        {
+            PreferencesPanel.SetActive(false);
+        }
+    }
+
+    public void SoundeffectToggleCheck()
+    {
+        if (AudioScript.activeSelf == false)
+        {
+            AudioScript.SetActive(true);
+        }
+        else
+        {
+            AudioScript.SetActive(false);
+        }
+    }
+
+
+
+
+}
--- Mega Tik Tack Toe/Assets/Scripts/StartmenuScript.cs
+++ Mega Tik Tack Toe/Assets/Scripts/StartmenuScript.cs
@@ -2,37 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 public class StartmenuScript : MonoBehaviour
 {
     public GameObject PreferencesWheel;
     public int Rotate = 1; //Start value of PreferencesWheelRotation
 
 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
     // Update is called once per frame
     void Update()
     {
-        PreferencesWheelRotation();
+
+        preferencesWheelRotation();
     }
 
 
 
     //Wheel Rotation
-    void PreferencesWheelRotation()
+    void preferencesWheelRotation()
     {
         Rotate--;
         if (Rotate == -360)
         {
             Rotate = 1;
         }
-        Debug.Log(Rotate);
         PreferencesWheel.transform.rotation = Quaternion.Euler(0, 0, Rotate);
     }
+
+    //Auf Knopfdruck "Start Game" startet das Spiel
+    public void switchScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
 
 }
 
