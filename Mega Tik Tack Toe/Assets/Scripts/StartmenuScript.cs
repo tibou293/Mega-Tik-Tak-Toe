8ec5314a2f013160feb18f0214c5701f033ce807
tibou293 <ti@schneider-dratz.de>
2021-04-07 20:43:34 +0200

    Wheel rotation
    Rotation added to preferences wheel

--- Mega Tik Tack Toe/Assets/Scripts/StartmenuScript.cs
+++ Mega Tik Tack Toe/Assets/Scripts/StartmenuScript.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartmenuScript : MonoBehaviour
 {
+    public GameObject PreferencesWheel;
+    public int Rotate = 1; //Start value of PreferencesWheelRotation
+
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        PreferencesWheelRotation();
     }
+
+
+
+    //Wheel Rotation
+    void PreferencesWheelRotation()
+    {
+        Rotate--;
+        if (Rotate == -360)
+        {
+            Rotate = 1;
+        }
+        Debug.Log(Rotate);
+        PreferencesWheel.transform.rotation = Quaternion.Euler(0, 0, Rotate);
+    }
+
 }
+
+
