2c98093aaab1c3294efbd35d543067cc5e93bdf4
tibou293 <ti@schneider-dratz.de>
2021-04-12 21:42:47 +0200

    Startmenu design

--- Mega Tik Tack Toe/Assets/Scripts/LightManager.cs
+++ Mega Tik Tack Toe/Assets/Scripts/LightManager.cs
@@ -8,23 +8,36 @@ public class LightManager : MonoBehaviour
     public float TimeOfDay;
 
     public Light Sun;
-
+    public Light Moon;
+    public float orbitSpeed = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        TimeOfDay = 8f;
     }
 
 
     // Update is called once per frame
     void Update()
     {
+        TimeOfDay += Time.deltaTime * orbitSpeed;
+        if (TimeOfDay > 24)
+        {
+            TimeOfDay = 0;
+        }
+        
         UpdateTime();
+
+
     }
 
     void UpdateTime()
     {
-        Sun.transform.rotation = Quaternion.Euler(TimeOfDay, 1079, 0);
+        float alpha = TimeOfDay / 24f;
+        float sunRotation = Mathf.Lerp(-90, 270, alpha);
+        float moonRotation = sunRotation - 180;
+        Sun.transform.rotation = Quaternion.Euler(sunRotation, 1079, 0);
+        Moon.transform.rotation = Quaternion.Euler(moonRotation, 1079, 0);
     }
 
 
